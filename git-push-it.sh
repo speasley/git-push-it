88c705ff7964e63958f440207e32fbb8b15328ce
Stephen Peasley <speasley@users.noreply.github.com>
2024-02-09 23:02:14 -0700

    refactor: play audio asynchronously

--- git-push-it.sh
+++ git-push-it.sh
@@ -20,18 +20,18 @@ git() {
 
 play_audio_linux() {
     if command -v mpg123 >/dev/null 2>&1; then
-        mpg123 "$AUDIO_FILE"
+        mpg123 "$AUDIO_FILE" >/dev/null 2>&1 &
     elif command -v aplay >/dev/null 2>&1; then
-        aplay "$AUDIO_FILE"
+        aplay "$AUDIO_FILE" >/dev/null 2>&1 &
     else
         echo "Sorry, no compatible Linux audio player was found."
     fi
 }
 
 play_audio_mac() {
-    afplay "$AUDIO_FILE"
+    afplay "$AUDIO_FILE" >/dev/null 2>&1 &
 }
 
 play_audio_windows() {
-    powershell -c "(New-Object Media.SoundPlayer \"$AUDIO_FILE\").PlaySync();"
+    powershell -c "Start-Process powershell -ArgumentList '-c (New-Object Media.SoundPlayer \"'$AUDIO_FILE'\").PlaySync();' "
 }
